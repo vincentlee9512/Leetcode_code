c67eba0cee4bbba6233b1d7af8df395d088e6ad7
vincentlee9512 <li000053@stthomas.edu>
2019-01-09 11:43:36 +0800

    update

--- src/Flipping_image_832.java
+++ src/Flipping_image_832.java
@@ -6,7 +6,7 @@ public class Flipping_image_832 {
      * To flip an image horizontally means that each row of the image is reversed.  For example, flipping [1, 1, 0] horizontally results in [0, 1, 1].
      *
      * To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0. For example, inverting [0, 1, 1] results in [1, 0, 0].
-     * @param image as 2-D array
+     *
      * @return flipped image as 2-D array
      */
 
